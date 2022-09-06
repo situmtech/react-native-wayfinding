60184682071a3ed571091766300984a8984c580a
Fran Dieguez <fran.dieguez@situm.com>
2022-09-06 12:57:39 +0200

    Improve example app to use ES6

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { MapView } from '@situm/react-native-wayfinding';
+import MapView from '@situm/react-native-wayfinding';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,55 +11,56 @@ const styles = StyleSheet.create({
   mapview: {
     width: '100%',
     height: '100%',
-  }
+  },
 });
 
 const App: React.FC = () => {
-
   const onMapReady = (event: any) => {
-    console.log("Map is ready now" + event);
-  }
+    console.log('Map is ready now' + event);
+  };
 
   const onFloorChange = (event: any) => {
-    console.log("on floor change detected: " + event);
-  }
+    console.log('on floor change detected: ' + event);
+  };
 
   const onPoiSelected = (event: any) => {
-    console.log("on poi selected detected: " + event);
-  }
+    console.log('on poi selected detected: ' + event);
+  };
 
   const onPoiDeselected = (event: any) => {
-    console.log("on poi deselected detected: " + event);
-  }
+    console.log('on poi deselected detected: ' + event);
+  };
 
   const onNavigationRequested = (event: any) => {
-    console.log("on navigation requested detected: " + event);
-  }
+    console.log('on navigation requested detected: ' + event);
+  };
 
   const onNavigationError = (event: any) => {
-    console.log("on navigation error detected: " + event);
-  }
+    console.log('on navigation error detected: ' + event);
+  };
 
   const onNavigationFinished = (event: any) => {
-    console.log("on navigation finished detected: " + event);
-  }
+    console.log('on navigation finished detected: ' + event);
+  };
 
-  return <View style={styles.container}>
-    <MapView style={styles.mapview}
-      user="SITUM_USER"
-      apikey="SITUM_APIKEY"
-      googleApikey="GOOGLE_MAPS_APIKEY"
-      buildingId="PUT_THE_BUILDING_IDENTIFIER_HERE"
-      onMapReadyCallback={this.onMapReady}
-      onFloorChangeCallback={this.onFloorChange}
-      onPoiSelectedCallback={this.onPoiSelected}
-      onPoiDeselectedCallback={this.onPoiDeselected}
-      onNavigationRequestedCallback={this.onNavigationRequested}
-      onNavigationError={this.onNavigationError}
-      onNavigationfinished={this.onNavigationFinished}
-    />
-  </View>
-
-}
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapview}
+        user="SITUM_USER"
+        apikey="SITUM_APIKEY"
+        googleApikey="GOOGLE_MAPS_APIKEY"
+        buildingId="PUT_THE_BUILDING_IDENTIFIER_HERE"
+        onMapReady={onMapReady}
+        onFloorChange={onFloorChange}
+        onPoiSelected={onPoiSelected}
+        onPoiDeselected={onPoiDeselected}
+        onNavigationRequested={onNavigationRequested}
+        onNavigationError={onNavigationError}
+        onNavigationFinished={onNavigationFinished}
+      />
+    </View>
+  );
+};
 
 export default App;
