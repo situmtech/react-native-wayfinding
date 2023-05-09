98c65083c0c8c4e33ec50b82ffeb9dd5a0ed01a6
Rodrigo Lago <rodrigo.lago@situm.com>
2023-05-09 15:47:52 +0200

    Fix swift files

--- ios/SitumWayfindingPlugin.swift
+++ ios/SitumWayfindingPlugin.swift
@@ -8,7 +8,7 @@ class SitumWayfindingPlugin: NSObject {
         if let wayfinding = MapView.library {
             let poiId = poiDict["id"] as! String
             let buildingId = poiDict["buildingId"] as! String
-            SITCommunicationManager.fetchPoiFromBuilding(poiId: poiId, buildingId: buildingId, completion: { result in
+            SITCommManagerAdapter.fetchPoiFromBuilding(poiId: poiId, buildingId: buildingId, completion: { result in
                 switch result {
                 case .success(let poi):
                     wayfinding.navigateToPoi(poi: poi)
