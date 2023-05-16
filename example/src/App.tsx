37aa99adfa05e640c3fe80e1bc1ce5c536a760cd
Damian Cruz <damian.cruz@situm.com>
2023-05-16 10:08:11 +0200

    fixing example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,92 @@
 import React from 'react';
-import MapViewScreen from './MapViewScreen';
-import { SitumProvider } from '@situm/react-native-wayfinding';
+import { StyleSheet } from 'react-native';
 
-export const SITUM_EMAIL = 'YOUR EMAIL HERE';
-export const SITUM_API_KEY = 'YOUR APIKEY HERE';
+import {
+  MapView,
+  OnFloorChangedResult,
+  OnNavigationResult,
+  OnPoiDeselectedResult,
+  OnPoiSelectedResult,
+  SitumProvider,
+  WayfindingResult,
+} from '@situm/react-native-wayfinding';
+
+const SITUM_EMAIL = 'YOUR_EMAIL_HERE';
+const SITUM_API_KEY = 'YOUR_APIKEY_HERE';
+const SITUM_BUILDING_ID = 'YOUR_BUILDING_ID_HERE';
+const SITUM_BASE_DOMAIN = 'https://map-viewer.situm.com';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapview: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 const App: React.FC = () => {
+  const onMapReady = (event: WayfindingResult) => {
+    console.log('Map is ready now' + JSON.stringify(event));
+  };
+
+  const onFloorChanged = (event: OnFloorChangedResult) => {
+    console.log('on floor change detected: ' + JSON.stringify(event));
+  };
+
+  const onPoiSelected = (event: OnPoiSelectedResult) => {
+    console.log('on poi selected detected: ' + JSON.stringify(event));
+  };
+
+  const onPoiDeselected = (event: OnPoiDeselectedResult) => {
+    console.log('on poi deselected detected: ' + JSON.stringify(event));
+  };
+
+  const onNavigationRequested = (event: OnNavigationResult) => {
+    console.log('on navigation requested detected: ' + JSON.stringify(event));
+  };
+
+  const onNavigationStarted = (event: OnNavigationResult) => {
+    console.log('on navigation started detected: ' + JSON.stringify(event));
+  };
+
+  const onNavigationError = (event: OnNavigationResult) => {
+    console.log('on navigation error detected: ' + JSON.stringify(event));
+  };
+
+  const onNavigationFinished = (event: OnNavigationResult) => {
+    console.log('on navigation finished detected: ' + JSON.stringify(event));
+  };
+
   return (
     <SitumProvider email={SITUM_EMAIL} apiKey={SITUM_API_KEY}>
-      <MapViewScreen />
+      <MapView
+        domain={SITUM_BASE_DOMAIN}
+        building={null}
+        style={styles.mapview}
+        user={SITUM_EMAIL}
+        apikey={SITUM_API_KEY}
+        googleApikey="GOOGLE_MAPS_APIKEY"
+        buildingId={SITUM_BUILDING_ID}
+        onMapReady={onMapReady}
+        onFloorChanged={onFloorChanged}
+        onPoiSelected={onPoiSelected}
+        onPoiDeselected={onPoiDeselected}
+        onNavigationRequested={onNavigationRequested}
+        onNavigationStarted={onNavigationStarted}
+        onNavigationError={onNavigationError}
+        onNavigationFinished={onNavigationFinished}
+        enablePoiClustering={true}
+        showPoiNames={true}
+        useRemoteConfig={true}
+        initialZoom={18} // Allowed range is [15-21]. Finally max(initialZoom, minZoom).
+        maxZoom={21} // [15-21]
+        minZoom={16} // [15-21]
+        useDashboardTheme={true}
+      />
     </SitumProvider>
   );
 };
